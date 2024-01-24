521ec3f0097f38f55e103aebc476a082e5a52b36
GarrixA <aphrogarrix10@gmail.com>
2024-01-24 19:11:23 +0200

    the cw

--- src/Components/Bars/SideBar.jsx
+++ src/Components/Bars/SideBar.jsx
@@ -1,58 +1,82 @@
 import "../../index.scss";
-import { Link, NavLink, Navigate, Outlet} from "react-router-dom";
+import { Link, NavLink, Navigate, Outlet } from "react-router-dom";
 import image from "../../images/RAB_Logo2.png";
 import { RiDashboardFill } from "react-icons/ri";
 import { LuMilk } from "react-icons/lu";
 import { HiBuildingOffice2 } from "react-icons/hi2";
 import { MdLogout } from "react-icons/md";
 import { IoMdPerson } from "react-icons/io";
+import { MdMenu } from "react-icons/md";
+import { useState } from "react";
 
 const SideBar = () => {
 
-  let auth = {'token' : true}
-  
+  let auth = { 'token': true }
+
   const navigations = [
-    {icon: <RiDashboardFill />, name: "Dashboard", href: "/dashboard", style: {
-      color: 'white', 
-    } },
-    {icon: <LuMilk />, name: "Production", href: "/dashboard/production"},
-    {icon: <HiBuildingOffice2 />, name: "MCCs", href: "/dashboard/mccs"},
-    {icon: <IoMdPerson />, name: "Veternaries", href: "/dashboard/veternaries"},
-    {icon: <MdLogout />, name: "Log Out", href: "/login",style: {color: 'red', fontStyle: 'italic'}},
+    { icon: <RiDashboardFill />, name: "Dashboard", href: "/dashboard" },
+    { icon: <LuMilk />, name: "Production", href: "/dashboard/production" },
+    { icon: <HiBuildingOffice2 />, name: "MCCs", href: "/dashboard/mccs" },
+    { icon: <IoMdPerson />, name: "Veternaries", href: "/dashboard/veternaries" },
+    { icon: <MdLogout />, name: "Log Out", href: "/login" },
   ]
+
+  const [openModal, setOpenModal] = useState(false)
+  const toggleModal = ()=>{
+    setOpenModal(!openModal);
+  }
 
   return (
     <div className="wrapper flex relative ">
       {/* left side */}
-      <div className="leftSide w-[15%] h-screen flex flex-col  fixed left-0 bg-[#009048]">
-         <div className="log flex items-center pt-2 px-2 space-x-2 border border-b-black bg-[#f1f1fb] pb-11"> 
-          <div className="img w-[50%] mt-5 mx-10 mb-0">
-          <Link to={"/"}><img src={image} alt="img" className=" object-contain" /></Link>
+      <div className="leftSide w-[15%] h-screen flex-col  fixed left-0 shadow-xl bg-[#009048] md:flex hidden">
+        <div className="log flex items-center py-4 px-6 space-x-2 border border-b-black bg-[#f1f1fb] pb-11">
+          <div className="img w-[40%]">
+            <Link to={"/"}><img src={image} alt="img" className=" object-contain" /></Link>
           </div>
-          {/* <div className="text font-bold uppercase">
+           <div className="text font-bold uppercase">
             <span className="text-[1.3rem]">Admin</span>
-          </div> */}
-        </div> 
-        <div className="links flex flex-col space-y-4 text-[1.2rem] font-semibold px-6 bg-[#009048]">
-          {navigations.map((item, idx) =>{
-           return( <NavLink 
-            key={idx}
-            to={item.href}
-            className="flex items-center space-x-4 mt-4">
-              <span className=" cursor-pointer space-x-4 text-white">{item.icon}</span>
-              <span className=" cursor-pointer space-x-4 text-white">{item.name}</span>
+          </div>
+        </div>
+        <div className="links text-white flex flex-col space-y-4 text-[1.2rem] font-semibold px-6 bg-[#009048]">
+          {navigations.map((item, idx) => {
+            return (<NavLink
+              key={idx}
+              to={item.href}
+              className="flex items-center space-x-4 mt-4">
+              <span className=" cursor-pointer space-x-4">{item.icon}</span>
+              <span className=" cursor-pointer space-x-4">{item.name}</span>
             </NavLink>
-          )})}
+            )
+          })}
         </div>
       </div>
       {/* Right side */}
-      <div className="rightSide w-[85%] h-screen absolute right-0 ">
-        <div className="top h-16 w-full bg-[#009048] fixed flex items-center z-30">
-        <span className="text-[1.3rem] text-white">ADMIN</span>
+      <div className="rightSide h-screen absolute md:w-[85%] w-full md:right-0">
+        <div className="top w-full fixed flex items-center z-30 md:bg-[#f1f1fb] bg-[#009048] md:h-16 md:p-0 p-2">
+          <div className="img w-[20%] md:hidden ">
+            <Link to={"/"}><img src={image} alt="img" className=" object-contain" /></Link>
+          </div>
+          <h1 className="text-2xl font-semibold md:mx-10 mx-2">Hi Admin</h1>
+          <MdMenu className="absolute right-0 text-white text-4xl mx-5" onClick={toggleModal}/>
         </div>
         {/* <Outlet /> */}
-        {auth.token ? <Outlet/> : <Navigate to={"/login"}/>}
+        {auth.token ? <Outlet /> : <Navigate to={"/login"} />}
       </div>
+      {openModal && (
+        <div className="links md:hidden absolute w-full top-[5.6rem] h-screen text-white flex flex-col space-y-4 text-[2rem] font-semibold px-6 bg-[#009048] ">
+        {navigations.map((item, idx) => {
+          return (<NavLink
+            key={idx}
+            to={item.href}
+            className="flex items-center space-x-4 mt-24">
+            <span className=" cursor-pointer space-x-4" onClick={toggleModal}>{item.icon}</span>
+            <span className=" cursor-pointer space-x-4" onClick={toggleModal}>{item.name}</span>
+          </NavLink>
+          )
+        })}
+      </div>
+      )}
     </div>
   );
 };
